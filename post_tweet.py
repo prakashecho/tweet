47df9fb0e41b570ef55fb608b4ddf84206db086d
prakashecho <50212384+prakashecho@users.noreply.github.com>
2024-10-29 20:18:10 +0530

    Update post_tweet.py

--- post_tweet.py
+++ post_tweet.py
@@ -1,11 +1,15 @@
-# post_tweet.py
 import os
 import tweepy
+import json
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler(os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"])
-auth.set_access_token(os.environ["TWITTER_ACCESS_TOKEN"], os.environ["TWITTER_ACCESS_TOKEN_SECRET"])
-api = tweepy.API(auth)
+client = tweepy.Client(
+    bearer_token=os.getenv("TWITTER_BEARER_TOKEN"),
+    consumer_key=os.getenv("TWITTER_API_KEY"),
+    consumer_secret=os.getenv("TWITTER_API_SECRET"),
+    access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+    access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+)
 
 # Load tweets from file
 with open('tweets.txt', 'r') as file:
@@ -14,8 +18,15 @@ with open('tweets.txt', 'r') as file:
 # Post first tweet and remove it from the file
 if tweets:
     tweet = tweets[0].strip()
-    api.update_status(tweet)
     
-    # Update the file to remove the posted tweet
-    with open('tweets.txt', 'w') as file:
-        file.writelines(tweets[1:])
+    # Use the v2 endpoint to post the tweet
+    response = client.create_tweet(text=tweet)
+    
+    if response.data:
+        print(f"Successfully posted tweet: {response.data['text']}")
+        
+        # Update the file to remove the posted tweet
+        with open('tweets.txt', 'w') as file:
+            file.writelines(tweets[1:])
+    else:
+        print("Failed to post tweet:", response)
